f40720739ad0cc6a6f11d05fcda78f6421fba1b5
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2018-07-05 14:18:58 -0500

    principal fuera

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -20,18 +20,16 @@ export default class FormValidation extends ServiceHandler {
     let vf = this;
     vf.settings = settings;
     vf.$domElement = $(queryElement);
-    vf.validation();
+    vf.validation(settings);
   }
-  validation() {
+  validation(settings) {
     let fv = this;
     let $domElement = fv.$domElement.find('form').length ? fv.$domElement.find('form') : fv.$domElement;
-    fv.valid = $domElement.validate({
+    fv.valid = $domElement.validate($.extend({
       normalizer: true,
       errorElement: 'p',
       errorClass: 'is-invalid',
       validClass: 'is-valid active',
-      rules: fv.settings.rules,
-      messages: fv.settings.messages,
       errorPlacement: (error, element) => {
         element.closest('.form-group').append(error.addClass('text-danger pl-3'));
       },
@@ -43,7 +41,7 @@ export default class FormValidation extends ServiceHandler {
         let igi = $(element).closest('.input-group-icon');
         (igi.length > 0 ? igi : $(element)).removeClass(errorClass).addClass(validClass);
       }
-    });
+    }, settings));
     $domElement.on('change', 'input, select, textarea', (e) => {
       $domElement.find('[type=submit]').prop('disabled', !$domElement.valid());
     });
@@ -52,49 +50,6 @@ export default class FormValidation extends ServiceHandler {
       fv.st = setTimeout(() => {
         $domElement.find('[type=submit]').prop('disabled', !$domElement.valid());
       }, 300);
-    });
-  }
-  userUpdate(url, data) {
-    let fv = this;
-    //url += '/' + fv.$domElement[0].email.value;
-    let token = fv.getToken();
-    let req = fv.ajax(url, data, { 'Authorization': token });
-    req.fail((jqXHR, textStatus, errorThrown) => {
-      var errorData = jqXHR.responseJSON || {};
-      //{"error":"invalid_grant","error_description":"Error"}
-      switch (jqXHR.status) {
-        case 200:
-          fv.doneUserSelect();
-          break;
-        case 304:
-          fv.errorUserUpdate();
-          break;
-        case 404:
-          fv.errorUserEmail();
-          break;
-        case 500: //se especifica que el error 500 va a dar al default
-        default:
-          fv.error(jqXHR.status, errorData.error_description || textStatus || 'Error', null, jqXHR);
-          break;
-      }
-    }).done(function (resp, textStatus, jqXHR) {
-      if (jqXHR.status == 304) {
-        fv.errorUserEmail();
-      } else {
-        fv.doneUserSelect();
-      }
-    });
-  }
-  errorUserUpdate() {
-    let vf = this;
-    vf.valid.showErrors({
-      email: vf.settings.messages.errorUserUpdate
-    });
-  }
-  errorUserEmail() {
-    let vf = this;
-    vf.valid.showErrors({
-      email: vf.settings.messages.errorUserEmail
     });
   }
 }
--- scripts/service-handler.js
+++ scripts/service-handler.js
@@ -16,120 +16,33 @@ export default class ServiceHandler {
     sh.settings = settings;
     let $domElement = sh.$domElement = $(queryElement);
     if (sh.settings.submit) {
-      let submitH = (e) => {
-        e.preventDefault();
-        let data = $(e.target).serializeArray();
-        data = data.concat(sh.extraData);
-        if (typeof sh.settings.todo === 'function') {
-          sh.settings.todo(sh, data);
-        } else if (typeof sh.settings.todo === 'string') {
-          sh[sh.settings.todo](sh.url(sh.settings.todoUrl), data);
-        }
-      };
       if ($domElement.find('form').length) {
-        $domElement.on('submit', 'form', submitH);
+        $domElement.on('submit', 'form', function (e) {
+          sh.submit(e);
+        });
       } else {
-        $domElement.on('submit', submitH);
+        $domElement.on('submit', function (e) {
+          sh.submit(e);
+        });
       }
     }
   }
-  login(url, data) {
+  submit(e) {
+    let data;
     let sh = this;
-    let req = sh.ajax(url, data);
-    req.fail((jqXHR, textStatus, errorThrown) => {
-      var errorData = jqXHR.responseJSON || {};
-      //{"error":"invalid_grant","error_description":"Error"}
-      switch (jqXHR.status) {
-        case 400:
-        case 401:
-          sh.errorLogin();
-          break;
-        case 429:
-          //como la etiqueta seleccionada es un script no se puede hacer $().find en el contenido
-          let $template = $($('#excessAttempts').html());
-          sh.error(jqXHR.status, $template.filter('.title').html(), $template.filter('.message').html());
-          break;
-        case 500: //se especifica que el error 500 va a dar al default
-        default:
-          sh.error(jqXHR.status, errorData.error_description || textStatus || 'Error', null);
-          break;
-      }
-    }).done(function (resp) {
-      sh.doneLogin(resp);
-    });
-  }
-  signout() {
-    let sh = this;
-    //url += '/' + sh.$domElement[0].email.value;
-    let token = sh.getToken();
-    let req = sh.ajax(sh.url('signout'), null, { 'Authorization': token });
-    req.always(function () {
-      sh.removeToken();
-      location.assign(baseUrl + 'logout/');
-    });
-    return req;
-  }
-  userSelect(url) {
-    let sh = this;
-    let token = sh.getToken();
-    let req = sh.ajax(url, null, { 'Authorization': token });
-    req.then(function (data, status, res) {
-      if (data.EsEmail == 'S') {
-        sh.doneUserSelect(data);
-      } else {
-        res.responseJSON = data;
-        return $.Deferred().reject(res, 'Error', 'Found');
-      }
-    }).fail((jqXHR, textStatus, errorThrown) => {
-      var errorData = jqXHR.responseJSON || {};
-      //{"error":"invalid_grant","error_description":"Error"}
-      if (errorData.MailUnico) {
-        if (errorData.MailUnico == 'S') {
-          sh.errorUserSelectFound();
-        } else {
-          sh.errorUserselectCallcenter();
-        }
-      } else {
-        sh.error(jqXHR.status, errorData.error_description || textStatus || 'Error', null, jqXHR.responseJSON);
-      }
-    });
-  }
-  contracts() {
-    let sh = this;
-    //url += '/' + sh.$domElement[0].email.value;
-    let token = sh.getToken();
-    let req = sh.ajax(sh.url('contracts'), null, { 'Authorization': token });
-    req.fail((jqXHR, textStatus, errorThrown) => {
-      var errorData = jqXHR.responseJSON || {};
-      switch (jqXHR.status) {
-        case 401:
-          sh.errorToken();
-          break;
-        case 500: //se especifica que el error 500 va a dar al default
-        default:
-          sh.error(jqXHR.status, errorData.error_description || textStatus || 'Error');
-          break;
-      }
-    });
-    return req;
-  }
-  alias(url, data) {
-    let sh = this;
-    let token = sh.getToken();
-    let req = sh.ajax(sh.url('alias'), data, { 'Authorization': token });
-    req.fail((jqXHR, textStatus, errorThrown) => {
-      var errorData = jqXHR.responseJSON || {};
-      switch (jqXHR.status) {
-        case 401:
-          sh.errorToken();
-          break;
-        case 500: //se especifica que el error 500 va a dar al default
-        default:
-          sh.error(jqXHR.status, errorData.error_description || textStatus || 'Error');
-          break;
-      }
-    });
-    return req;
+    if (e.preventDefault && e.stopPropagation) {
+      e.preventDefault();
+      e.stopPropagation();
+      data = $(e.target).serializeArray();
+    } else {
+      data = e.data;
+    }
+    data = data.concat(sh.extraData);
+    if (typeof sh.settings.todo === 'function') {
+      sh.settings.todo(sh, data);
+    } else if (typeof sh.settings.todo === 'string') {
+      sh[sh.settings.todo](sh.url(sh.settings.todoUrl), data);
+    }
   }
   ajax(url, data, headers) {
     let sh = this;
@@ -149,61 +62,46 @@ export default class ServiceHandler {
     });
     return request;
   }
-  doneLogin(data, textStatus, jqXHR) {
-    this.setToken(data);
-    this.userSelect(this.url('userSelect'));
-  }
-  doneUserSelect(data, textStatus, jqXHR) {
-    sessionStorage.setItem('user', JSON.stringify(data));
-    location.assign(baseUrl + 'dashboard/');
-  }
-  errorLogin() {
-    this.$domElement.find('#error-msg').show();
-    let $input = this.$domElement.find('.input-group-icon,:not(.input-group-icon)>.form-control');
-    $input.addClass('is-invalid');
-  }
-  errorUserSelectFound() {
-    //TODO: el nombre de usaurio está estático!!!! hay que cambiarlo para mostrar el que venga en los datos
-    $('.modal').modal('hide');
-    $('#update-account').modal('show');
-  }
-  errorUserselectCallcenter() {
-    let $template = $($('#callCenter').html());
-    this.error(300, $template.filter('.title').html(), $template.filter('.message').html());
-  }
-  errorToken() {
-    this.removeToken();
-    location.assign(baseUrl + 'login/');
-  }
-  error(status, title, body, jsonResp) {
+  modal(status, title, body, jsonResp) {
     $('.modal').modal('hide');
     $('#errorCatcher').find('.modal-title span').text(title || 'Error');
     $('#errorCatcher').find('.modal-body .message').html(body || '<p>Código de error: ' + status + '</p>');
     if (jsonResp) {
       $('#errorCatcher').find('.modal-body .message').append($('<p></p>').jJsonViewer(jsonResp, { expanded: true }));
     }
-    $('#errorCatcher').modal('show');
+    setTimeout(() => {
+      $('#errorCatcher').modal('show');
+    }, 600);
   }
-  setToken(resp) {
-    resp.date = Date.now();
-    sessionStorage.setItem('token', JSON.stringify(resp));
+  modalTemplate(queryElement, status) {
+    let sh = this;
+    let $template = $($(queryElement).html());
+    sh.modal(status, $template.filter('.title').html(), $template.filter('.message').html());
   }
-  getToken() {
-    let resp = JSON.parse(sessionStorage.getItem('token'));
-    if (resp) {
-      return resp.token_type + ' ' + resp.access_token;
+  errorProcess(status, textStatus, errorData) {
+    let sh = this;
+    let title = 'Error';
+    if (typeof errorData === 'string') {
+      title = errorData;
+      errorData = false;
     } else {
-      return false;
+      title = textStatus;
     }
-  }
-  removeToken() {
-    sessionStorage.removeItem('token');
+    sh.modal(status, title, null, errorData);
   }
   url(id) {
-    if (id == 'base') {
-      return this.settings.urls.base;
+    if (typeof this.settings.urls[id] !== 'undefined') {
+      if (id == 'base') {
+        return this.settings.urls.base;
+      } else {
+        if (this.settings.urls[id].search(/^https?:\/\//) > -1) {
+          return this.settings.urls[id];
+        } else {
+          return this.settings.urls.base + this.settings.urls[id];
+        }
+      }
     } else {
-      return this.settings.urls.base + this.settings.urls[id];
+      console.log('error todoUrl "' + id + '" is not defined');
     }
   }
   loading(queryElement) {
